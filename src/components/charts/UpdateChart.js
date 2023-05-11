710a090a3de12a4e76d6c3a39ff76a7e31c0b663
changseoksong <a1373052011@gmail.com>
2023-05-11 16:48:44 +0900

    차트 업데이트 및 옵션 관련 테스트

--- src/components/charts/UpdateChart.js
+++ src/components/charts/UpdateChart.js
@@ -1,12 +1,65 @@
-import HighCharts from "highcharts";
+import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
+import { useState } from "react";
 
-const options = {};
+const UpdateChart = () => {
+  const [chartOptions, setChartOptions] = useState({
+    title: { text: "test chart" },
+    subtitle: { text: "subtitle chart" },
+    xAxis: { categories: ["A", "B", "C"], gridLineWidth: 1 },
+    yAxis: [
+      {
+        title: { text: "y 축" },
+        labels: {
+          formatter: ({ value }) => value + "%",
+        },
+        gridLineWidth: 1,
+      },
+      { title: { text: "y 축2" }, opposite: true },
+    ],
+    series: [
+      { yAxis: 0, data: [1, 2, 3] },
+      { yAxis: 1, data: [4, 5, 2] },
+    ],
+    plotOptions: {
+      series: {
+        point: {
+          events: {
+            mouseOver: ({ target: { category } }) => {
+              setHoverData(category);
+            },
+          },
+        },
+      },
+    },
+  });
+  const [hoverData, setHoverData] = useState();
 
-const UpdateChart = () => (
-  <div>
-    <HighchartsReact highcharts={Highcharts} options={options} />
-  </div>
-);
+  const updateSeriesHandler = () => {
+    setChartOptions((prev) => ({
+      ...prev,
+      series: [{ data: [Math.random() * 5, 2, 1] }],
+    }));
+  };
+  const callback = (chart) => {
+    console.log("chart", chart);
+  };
+
+  return (
+    <div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={chartOptions}
+        // immutable
+        containerProps={{ style: { width: "50%" } }}
+        callback={callback}
+      />
+      <h3>{hoverData}</h3>
+      <button type="button" onClick={updateSeriesHandler}>
+        Update Series
+      </button>
+    </div>
+  );
+};
 
 export default UpdateChart;
